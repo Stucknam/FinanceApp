b7ffee84e1451726a1b79972570c5eed1072324c
Алексей Панферов <ad.gm1526@gmail.com>
2026-05-06 16:34:19 +0300

    Добавление swagger

--- FinanceApp.Api/Program.cs
+++ FinanceApp.Api/Program.cs
@@ -24,6 +24,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 
@@ -33,6 +35,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
