8ef6298d4b622b8e3d7bde369df3b0c51cd2f90a
Алексей Панферов <ad.gm1526@gmail.com>
2026-05-06 15:14:07 +0300

    Добавление web api и настройка di

---
+++ FinanceApp.Api/DependencyInjection/ApiServiceRegistration.cs
@@ -0,0 +1,43 @@
+﻿using FinanceApp.Application.Services;
+using FinanceApp.Data;
+using FinanceApp.Data.Repositories;
+using FinanceApp.Domain.Interfaces.Repositories;
+using FinanceApp.Domain.Interfaces.Srevices;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace FinanceApp.Api.DependencyInjection
+{
+    public static class ApiServiceRegistration
+    {
+        public static IServiceCollection AddData(this IServiceCollection services, IConfiguration config)
+        {
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<FinanceDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<ITransferRepository, TransferRepository>();
+
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<ITransactionService, TransactionService>();
+            services.AddScoped<ITransferService, TransferService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IStatisticsService, StatisticsService>();
+
+            return services;
+        }
+    }
+}
---
+++ FinanceApp.Api/Program.cs
@@ -0,0 +1,44 @@
+using FinanceApp.Api.DependencyInjection;
+using FinanceApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = Directory.GetCurrentDirectory()
+});
+
+// Add services to the container.
+
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    //.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddData(builder.Configuration);
+builder.Services.AddApplication();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
